b03129d298190fbc2e65daa278249deec9904aba
elizapimentel <elizapimentel@hotmail.com>
2024-07-19 18:16:01 -0300

    add interfaces

---
+++ src/interfaces/ContaBancaria.ts
@@ -0,0 +1,7 @@
+
+export interface ContaBancaria {
+    depositar(valor: number): void;
+    sacar(valor: number): void;
+    transferir(valor: number, contaDestino: ContaBancaria): void;
+    consultarSaldo(): number;
+}
