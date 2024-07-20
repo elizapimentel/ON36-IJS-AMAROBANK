63653db420b724fd89a3860a3368b96c81f1f28e
elizapimentel <elizapimentel@hotmail.com>
2024-07-20 10:16:26 -0300

    add id

--- src/models/Transacao.ts
+++ src/models/Transacao.ts
@@ -1,18 +1,26 @@
 import { Conta } from './Conta';
 
 export class Transacao {
+    private id: number;
     private data: Date;
     private valor: number;
     private tipoTransacao: string;
     private contaOrigem: Conta;
     private contaDestino: Conta;
 
+    private static nextid = 1;
+
     constructor(data: Date, valor: number, tipoTransacao: string, contaOrigem: Conta, contaDestino: Conta) {
+        this.id = Transacao.nextid;
         this.data = data;
         this.valor = valor;
         this.tipoTransacao = tipoTransacao;
         this.contaOrigem = contaOrigem;
         this.contaDestino = contaDestino;
+    }
+
+    public getId(): number {
+        return this.id;
     }
 
     public consultarHistorico(): Transacao[] { 
