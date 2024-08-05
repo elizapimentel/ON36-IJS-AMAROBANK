86a07b2d14bd789913ce811b1267bd52f20324e6
elizapimentel <elizapimentel@hotmail.com>
2024-08-05 18:57:44 -0300

    refact: add enum tipoConta

--- src/contas/dto/create-conta.dto.ts
+++ src/contas/dto/create-conta.dto.ts
@@ -1,10 +1,11 @@
-import { Transacao } from "src/transacoes/entities/transacao.entity";
+import { Transacao } from 'src/transacoes/entities/transacao.entity';
+import { tipoConta } from '../entities/conta.entity';
 
 export class CreateContaDto {
-    readonly id: number;
-    readonly numeroConta: number;
-    readonly saldo: number; 
-    readonly tipoConta: string;
-    readonly transacoes: Transacao[];
-    // moeda
+  readonly id: number;
+  readonly numeroConta: number;
+  readonly saldo: number;
+  readonly tipoConta: tipoConta;
+  readonly transacoes: Transacao[];
+  // moeda
 }
--- src/contas/entities/conta.entity.ts
+++ src/contas/entities/conta.entity.ts
@@ -1,11 +1,16 @@
-import { Transacao } from "src/transacoes/entities/transacao.entity";
+import { Transacao } from 'src/transacoes/entities/transacao.entity';
+
+export enum tipoConta {
+  CORRENTE = 'corrente',
+  POUPANÇA = 'poupança',
+}
 
 export class Conta {
-    constructor(
-        public id: number,
-        public numeroConta: number,
-        public saldo: number,
-        public tipoConta: string,
-        public transacoes: Transacao[]
-    ){}
+  constructor(
+    public id: number,
+    public numeroConta: number,
+    public saldo: number,
+    public tipoConta: tipoConta,
+    public transacoes: Transacao[],
+  ) {}
 }
