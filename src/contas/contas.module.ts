3e8ce8f604d40662db1813d4a958a5706486c728
elizapimentel <elizapimentel@hotmail.com>
2024-08-06 22:19:18 -0300

    perf: add forwardRef to imports

--- src/contas/contas.module.ts
+++ src/contas/contas.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ContasService } from './contas.service';
-import { ContasController } from './contas.controller';
+import { forwardRef, Module } from '@nestjs/common';
+import { ContasService } from './services/contas.service';
+import { ContasController } from './controller/contas.controller';
+import { TransacoesModule } from 'src/transacoes/transacoes.module';
 
 @Module({
+  imports: [forwardRef(() =>TransacoesModule)],
   controllers: [ContasController],
   providers: [ContasService],
   exports: [ContasService],
