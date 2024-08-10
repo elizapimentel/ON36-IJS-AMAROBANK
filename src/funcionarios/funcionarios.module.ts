bbb1323b739bcddac3a83400767f088ccd8905d1
elizapimentel <elizapimentel@hotmail.com>
2024-08-10 17:25:33 -0300

    feat: add providers

--- src/funcionarios/funcionarios.module.ts
+++ src/funcionarios/funcionarios.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FuncionariosController } from './controller/funcionarios.controller';
 import { FuncionariosService } from './service/funcionarios.service';
-import { ClientesModule } from 'src/clientes/clientes.module';
+import { ClientesModule } from '../clientes/clientes.module';
+import { FuncionariosRepository } from './repository/funcionario.repository';
+import { FuncionariosFactory } from '../factories/funcionarios.factory';
 
 @Module({
   imports: [ClientesModule],
   controllers: [FuncionariosController],
-  providers: [FuncionariosService],
+  providers: [FuncionariosService, FuncionariosFactory, FuncionariosRepository],
   exports: [FuncionariosService],
 })
 export class FuncionariosModule {}
