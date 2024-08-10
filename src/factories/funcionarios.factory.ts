9cdf6f1d0e476f408830c638111136af1c94f922
elizapimentel <elizapimentel@hotmail.com>
2024-08-10 17:23:05 -0300

    refactor: add implementations

--- src/factories/funcionarios.factory.ts
+++ src/factories/funcionarios.factory.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { Cliente } from 'src/clientes/entities/cliente.entity';
-import { TipoCargo } from 'src/common/enums/tipo-.conta.enum';
-import { Agente } from 'src/funcionarios/entities/agente.entity';
-import { Funcionario } from 'src/funcionarios/entities/funcionario.entity';
-import { Gerente } from 'src/funcionarios/entities/gerente.entity';
+// import { Cliente } from '../clientes/entities/cliente.entity';
+import { TipoCargo } from '../common/enums/tipo-.banco.enum';
+import { Agente } from '../funcionarios/entities/agente.entity';
+import { Funcionario } from '../funcionarios/entities/funcionario.entity';
+import { Gerente } from '../funcionarios/entities/gerente.entity';
+import { CreateFuncionarioDto } from '../funcionarios/dto/create-funcionario.dto';
 
 @Injectable()
 export class FuncionariosFactory {
-  static criarFuncionario(
+  criarFuncionario(
     cargo: TipoCargo,
-    id: number,
-    nomeCompleto: string,
-    telefones: string[],
-    clientes?: Cliente[],
+    funcionario: CreateFuncionarioDto,
   ): Funcionario {
     switch (cargo) {
       case TipoCargo.AGENTE:
-        return new Agente(id, nomeCompleto, cargo, telefones);
+        return new Agente(
+          funcionario.cargo,
+          funcionario.nomeCompleto,
+          funcionario.endereco,
+          funcionario.telefones,
+        );
       case TipoCargo.GERENTE:
-        return new Gerente(id, nomeCompleto, cargo, telefones, clientes);
+        return new Gerente(
+          funcionario.cargo,
+          funcionario.nomeCompleto,
+          funcionario.endereco,
+          funcionario.telefones,
+          funcionario.clientes || [],
+        );
       default:
         throw new Error('Cargo inválido');
     }
