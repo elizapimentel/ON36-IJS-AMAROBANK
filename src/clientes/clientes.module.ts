87ec01714a763fb19fc8bbbd99d5e0debfa335f3
elizapimentel <elizapimentel@hotmail.com>
2024-08-14 16:22:54 -0300

    refact: update modules

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ClientesService } from './service/clientes.service';
 import { ClientesController } from './controller/clientes.controller';
-// import { Contas } from '../contas/entities/conta.entity';
+import { Contas } from '../contas/entities/conta.entity';
 import { Gerente } from '../funcionarios/entities/gerente.entity';
 import { ClientesRepository } from './repository/clientes.repository';
 import { FuncionariosRepository } from '../funcionarios/repository/funcionario.repository';
 
 @Module({
   controllers: [ClientesController],
-  providers: [ClientesService, ClientesRepository, FuncionariosRepository],
+  providers: [ClientesService, ClientesRepository, FuncionariosRepository, Contas],
   exports: [ClientesService],
   imports: [Gerente],
 })
--- src/transacoes/transacoes.module.ts
+++ src/transacoes/transacoes.module.ts
@@ -2,11 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TransacoesController } from './controller/transacoes.controller';
 import { TransacoesService } from './service/transacoes.service';
 import { ContasModule } from 'src/contas/contas.module';
+import { TransacoesFactory } from '../factories/transacoes.factory';
+import { TransacoesRepository } from './repository/transacoes.repository';
 
 @Module({
-  imports: [forwardRef(() =>ContasModule)],
+  imports: [forwardRef(() => ContasModule)],
   controllers: [TransacoesController],
-  providers: [TransacoesService],
+  providers: [TransacoesService, TransacoesFactory, TransacoesRepository],
   exports: [TransacoesService],
 })
-export class TransacoesModule {}
+export class TransacoesModule { }
