c6ecaf3161831cf16abb1771ecd796befeae12f3
elizapimentel <elizapimentel@hotmail.com>
2024-08-14 16:26:56 -0300

    update module

--- src/contas/contas.module.ts
+++ src/contas/contas.module.ts
@@ -1,12 +1,16 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { ContasService } from './services/contas.service';
 import { ContasController } from './controller/contas.controller';
-import { TransacoesModule } from 'src/transacoes/transacoes.module';
+import { TransacoesModule } from '../transacoes/transacoes.module';
+import { TransacoesRepository } from '../transacoes/repository/transacoes.repository';
+import { ContasFactory } from '../factories/contas.factory';
+import { TransacoesFactory } from '../factories/transacoes.factory';
+import { ContasRepository } from './repository/contas.repository';
 
 @Module({
-  imports: [forwardRef(() =>TransacoesModule)],
+  imports: [forwardRef(() => TransacoesModule)],
   controllers: [ContasController],
-  providers: [ContasService],
+  providers: [ContasService, ContasRepository, TransacoesRepository, ContasFactory, TransacoesFactory],
   exports: [ContasService],
 })
-export class ContasModule {}
+export class ContasModule { }
