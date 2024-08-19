df0ed49ef710755aef2d754682c5e2f89756a79a
elizapimentel <elizapimentel@hotmail.com>
2024-08-19 15:45:41 -0300

    chore: add archs to each class

--- src/factories/contas/contas.factory.ts
+++ src/factories/contas/contas.factory.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { TipoConta } from '../common/enums/tipo-.banco.enum';
-import { ContaCorrente } from '../contas/entities/conta-corrente.entity';
-import { ContaPoupanca } from '../contas/entities/conta-poupanca.entity';
-import { Contas } from '../contas/entities/conta.entity';
-import { ContaPJ } from '../contas/entities/conta-pj.entity';
-import { CreateContaDto } from '../contas/dto/create/create-conta.dto';
-import { CreateContaCorrenteDto } from '../contas/dto/create/create-conta-corrente.dto';
-import { CreateContaPoupancaDto } from '../contas/dto/create/create-conta-poupanca.dto';
-import { CreateContaPJDto } from '../contas/dto/create/create-conta-pj.dto';
+import { TipoConta } from '../../common/enums/tipo-.banco.enum';
+import { CreateContaCorrenteDto } from '../../contas/dto/create/create-conta-corrente.dto';
+import { CreateContaPJDto } from '../../contas/dto/create/create-conta-pj.dto';
+import { CreateContaPoupancaDto } from '../../contas/dto/create/create-conta-poupanca.dto';
+import { CreateContaDto } from '../../contas/dto/create/create-conta.dto';
+import { ContaCorrente } from '../../contas/entities/conta-corrente.entity';
+import { ContaPJ } from '../../contas/entities/conta-pj.entity';
+import { ContaPoupanca } from '../../contas/entities/conta-poupanca.entity';
+import { Contas } from '../../contas/entities/conta.entity';
+
 
 @Injectable()
 export class ContasFactory {
