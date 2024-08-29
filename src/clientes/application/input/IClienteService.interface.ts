717349f0b871d5d128315e91e0c9494735a663b2
elizapimentel <elizapimentel@hotmail.com>
2024-08-29 18:44:07 -0300

    refactor: change methods to receive entity and add adicionarConta method

--- src/clientes/application/input/IClienteService.interface.ts
+++ src/clientes/application/input/IClienteService.interface.ts
@@ -1,10 +1,13 @@
 import { UpdateClienteDto } from "../../../clientes/infra/adapters/inbound/dto/update-cliente.dto";
-import { Cliente } from "../../domain/entities/cliente.entity";
+import { ClienteEntity } from "../../../clientes/infra/adapters/entities/cliente.entity";
+import { ContaEntity } from "../../../contas/infra/adapters/entities/conta.entity";
 
 
 export interface IClienteService {
-    listarTodos(): Cliente[];
-    encontrarPorId(id: string): Cliente;
-    atualizarCliente(id: string, updateClienteDto: UpdateClienteDto): Cliente;
-    removerCliente(id: string): void;
+    cadastrarCliente(funcionario: any, criarClienteDto: any): Promise<ClienteEntity>;
+    listarTodos(): Promise<ClienteEntity[]>;
+    encontrarPorId(id: string): Promise<ClienteEntity>;
+    atualizarCliente(id: string, updateClienteDto: UpdateClienteDto): Promise<ClienteEntity>;
+    removerCliente(id: string): Promise<void>;
+    adicionarConta(clienteId: string, contas: ContaEntity): Promise<ClienteEntity>;
 }
--- src/clientes/application/input/clientes.service.ts
+++ src/clientes/application/input/clientes.service.ts
@@ -1,76 +1,84 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { IClienteService } from './IClienteService.interface';
-import { Cliente } from '../../../clientes/domain/entities/cliente.entity';
 import { ClientesRepository } from '../../../clientes/infra/adapters/outbound/repository/clientes.repository';
 import { CreateClienteDto } from '../../../clientes/infra/adapters/inbound/dto/create-cliente.dto';
 import { UpdateClienteDto } from '../../../clientes/infra/adapters/inbound/dto/update-cliente.dto';
-import { TipoCargo } from '../../../common/enums/tipo-.banco.enum';
-import { Funcionario } from '../../../funcionarios/domain/entities/funcionario.entity';
-import { Gerente } from '../../../funcionarios/domain/entities/gerente.entity';
 import { FuncionariosRepository } from '../../../funcionarios/infra/adapters/outbound/repository/funcionario.repository';
-
+import { ClienteEntity } from '../../../clientes/infra/adapters/entities/cliente.entity'; 
+import { FuncionarioEntity } from '../../../funcionarios/infra/adapters/entities/funcionario.entity'; 
+import { GerenteEntity } from '../../../funcionarios/infra/adapters/entities/gerente.entity';
+import { TipoCargo } from '../../../common/enums/tipo-.banco.enum';
+import { ContaEntity } from '../../../contas/infra/adapters/entities/conta.entity';
 
 
 @Injectable()
 export class ClientesService implements IClienteService {
-  public clientes: Cliente[] = [];
-
   constructor(
     private clienteRepo: ClientesRepository,
     private funcionarioRepo: FuncionariosRepository,
   ) { }
 
-
   async cadastrarCliente(
-    funcionario: Funcionario,
-    criarClienteDto: CreateClienteDto,
-  ): Promise<Cliente> {
+    funcionario: FuncionarioEntity,
+    criarClienteDto: CreateClienteDto
+  ): Promise<ClienteEntity> {
     if (funcionario.cargo !== TipoCargo.GERENTE) {
       throw new NotFoundException(`Funcionário não autorizado`);
     }
 
     const { nomeCompleto, endereco, telefones, gerenteId } = criarClienteDto;
-
-    const novoCliente = new Cliente(nomeCompleto, endereco, telefones);
+    const novoCliente = new ClienteEntity();
+    novoCliente.nomeCompleto = nomeCompleto;
+    novoCliente.endereco = endereco;
+    novoCliente.telefones = telefones;
 
     if (gerenteId) {
-      const gerente = await this.funcionarioRepo.buscarGerentePorId(gerenteId);
-      if (gerente instanceof Gerente) {
+      const gerente = await this.funcionarioRepo.findOne({
+        where: { id: gerenteId },
+      });
+      if (gerente instanceof GerenteEntity) {
         novoCliente.gerente = gerente;
       }
     }
-    return this.clienteRepo.cadastrarCliente(novoCliente);
+    return this.clienteRepo.save(novoCliente);
   }
 
-  listarTodos(): Cliente[] {
-    return this.clienteRepo.listarTodos();
+  async listarTodos(): Promise<ClienteEntity[]> {
+    return this.clienteRepo.find();
   }
 
-  encontrarPorId(id: string): Cliente {
-    const cliente = this.clienteRepo.encontrarPorId(id);
+  async encontrarPorId(id: string): Promise<ClienteEntity> {
+    const cliente = await this.clienteRepo.findOne({
+       where: { id: id },
+    });
     if (!cliente) {
       throw new NotFoundException(`Cliente com id ${id} não encontrado`);
     }
     return cliente;
   }
 
-  atualizarCliente(id: string, updateClienteDto: UpdateClienteDto): Cliente {
-    const cliente = this.clienteRepo.encontrarPorId(id);
-    if (!cliente) {
-      throw new NotFoundException(`Cliente com id ${id} não encontrado`);
-    }
-
+  async atualizarCliente(id: string, updateClienteDto: UpdateClienteDto): Promise<ClienteEntity> {
+    const cliente = await this.encontrarPorId(id);
     const clienteAtualizado = {
       ...cliente,
       ...updateClienteDto,
     };
 
-    return this.clienteRepo.salvar(clienteAtualizado);
-  };
-
-  removerCliente(id: string): void {
-    const cliente = this.encontrarPorId(id);
-    this.clienteRepo.removerCliente(cliente.id);
+    return this.clienteRepo.save(clienteAtualizado);
   }
 
+  async removerCliente(id: string): Promise<void> {
+    const cliente = await this.encontrarPorId(id);
+    await this.clienteRepo.remove(cliente);
+  }
+
+  async adicionarConta(clienteId: string, conta: ContaEntity): Promise<ClienteEntity> {
+    const cliente = await this.encontrarPorId(clienteId);
+    if (!cliente) {
+      throw new NotFoundException(`Cliente com id ${clienteId} não encontrado`);
+    }
+
+    cliente.contas.push(conta);
+    return this.clienteRepo.save(cliente);
+  }
 }
