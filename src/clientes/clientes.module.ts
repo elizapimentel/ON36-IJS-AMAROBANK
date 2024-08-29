a2f7c1b58a9cd31e81f4c5015d5eeaec73cd8049
elizapimentel <elizapimentel@hotmail.com>
2024-08-29 18:31:23 -0300

    refactor: update imports to include Type Orm Modules

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ClientesRepository } from './infra/adapters/outbound/repository/clientes.repository';
 import { Contas } from '../contas/domain/models/conta';
-import { Gerente } from '../funcionarios/domain/entities/gerente.entity';
+import { Gerente } from '../funcionarios/domain/models/gerente.entity
 import { FuncionariosRepository } from '../funcionarios/infra/adapters/outbound/repository/funcionario.repository';
 import { ClientesService } from './application/input/clientes.service';
 import { ClientesController } from './infra/adapters/inbound/controller/clientes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cliente } from './domain/models/cliente';
 
 @Module({
   controllers: [ClientesController],
   providers: [ClientesService, ClientesRepository, FuncionariosRepository],
   exports: [ClientesService],
-  imports: [Gerente, Contas],
+  imports: [
+    TypeOrmModule.forFeature([Cliente, Contas, Gerente])
+  ],
 })
 export class ClientesModule { }
