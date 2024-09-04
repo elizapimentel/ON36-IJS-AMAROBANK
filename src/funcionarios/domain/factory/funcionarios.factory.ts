11fdab12c5a34dd469de038fcf6000c809d6dc5d
elizapimentel <elizapimentel@hotmail.com>
2024-09-04 17:18:27 -0300

    refactor: change model classes into entities

--- src/funcionarios/domain/factory/funcionarios.factory.ts
+++ src/funcionarios/domain/factory/funcionarios.factory.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { TipoCargo } from '../../../common/enums/tipo-.banco.enum';
 import { CreateFuncionarioDto } from '../../../funcionarios/infra/adapters/inbound/dto/create-funcionario.dto';
-import { Agente } from '../entities/agente.entity';
-import { Funcionario } from '../entities/funcionario.entity';
-import { Gerente } from '../entities/gerente.entity';
+import { AgenteEntity } from 'src/funcionarios/infra/adapters/entities/agente.entity';
+import { FuncionarioEntity } from 'src/funcionarios/infra/adapters/entities/funcionario.entity';
+import { GerenteEntity } from 'src/funcionarios/infra/adapters/entities/gerente.entity';
 
 
 
@@ -12,23 +12,21 @@ export class FuncionariosFactory {
   criarFuncionario(
     cargo: TipoCargo,
     funcionario: CreateFuncionarioDto,
-  ): Funcionario {
+  ): FuncionarioEntity {
     switch (cargo) {
       case TipoCargo.AGENTE:
-        return new Agente(
-          funcionario.cargo,
-          funcionario.nomeCompleto,
-          funcionario.endereco,
-          funcionario.telefones,
-        );
+        return new AgenteEntity({
+          nomeCompleto: funcionario.nomeCompleto,
+          endereco: funcionario.endereco,
+          telefones: funcionario.telefones,
+        });
       case TipoCargo.GERENTE:
-        return new Gerente(
-          funcionario.cargo,
-          funcionario.nomeCompleto,
-          funcionario.endereco,
-          funcionario.telefones,
-          funcionario.clientes || [],
-        );
+        return new GerenteEntity({
+          nomeCompleto: funcionario.nomeCompleto,
+          endereco: funcionario.endereco,
+          telefones: funcionario.telefones,
+          clientes: funcionario.clientes || [],
+        });
       default:
         throw new Error('Cargo inválido');
     }
