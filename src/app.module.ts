8ab76ddc771038e4f159e6e0aab944253e5a5bcc
elizapimentel <elizapimentel@hotmail.com>
2024-09-04 16:57:56 -0300

    refactor: add TypeOrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContasModule } from './contas/contas.module';
-import { TransacoesModule } from './transacoes/transacoes.module';
+// import { ContasModule } from './contas/contas.module';
+// import { TransacoesModule } from './transacoes/transacoes.module';
 import { FuncionariosModule } from './funcionarios/funcionarios.module';
 import { ClientesModule } from './clientes/clientes.module';
 import { CnpjModule } from './cnpj/cnpj.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from 'data-source';
 
 @Module({
-  imports: [ClientesModule, FuncionariosModule, ContasModule, TransacoesModule, CnpjModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options, 
+      autoLoadEntities: true,
+    }), ClientesModule, FuncionariosModule, CnpjModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
