3469377b793c8272e86e6aeae2d0e1b3ad14663a
elizapimentel <elizapimentel@hotmail.com>
2024-09-04 17:14:42 -0300

    refactor: change to typeorm repository

--- src/clientes/infra/adapters/outbound/repository/clientes.repository.ts
+++ src/clientes/infra/adapters/outbound/repository/clientes.repository.ts
@@ -1,37 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { randomUUID as uuid } from 'crypto';
-import { Cliente } from '../../../../domain/entities/cliente.entity';
+import { Repository } from 'typeorm';
+import { ClienteEntity } from '../../entities/cliente.entity';
+
 
 @Injectable()
-export class ClientesRepository {
-  private clientes: Cliente[] = [];
+export class ClientesRepository extends Repository<ClienteEntity> {
 
-  constructor() {}
-
-  cadastrarCliente(cliente: Cliente): Cliente {
-    cliente.id = uuid();
-    this.clientes.push(cliente);
-    return cliente;
-  }
-
-  listarTodos(): Cliente[] {
-    return this.clientes;
-  }
-
-  encontrarPorId(id: string): Cliente {
-    const cliente = this.clientes.find((cliente) => cliente.id === id);
-    return cliente || null;
-  }
-
-  salvar(cliente: Cliente): Cliente {
-    this.clientes = this.clientes.map((c) =>
-      c.id === cliente.id ? cliente : c,
-    );
-    return cliente;
-  }
-
-  removerCliente(id: string): void {
-    const clienteIndex = this.clientes.findIndex((cliente) => cliente.id === id);
-    this.clientes.splice(clienteIndex, 1);
-  }
 }
