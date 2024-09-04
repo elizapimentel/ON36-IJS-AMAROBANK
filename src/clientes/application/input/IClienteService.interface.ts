e2a5de1c7798453bda7ae076a75f430687d137cd
elizapimentel <elizapimentel@hotmail.com>
2024-09-04 17:01:28 -0300

    refactor: update methods and add const to interface

--- src/clientes/application/input/IClienteService.interface.ts
+++ src/clientes/application/input/IClienteService.interface.ts
@@ -1,13 +1,13 @@
 import { UpdateClienteDto } from "../../../clientes/infra/adapters/inbound/dto/update-cliente.dto";
 import { ClienteEntity } from "../../../clientes/infra/adapters/entities/cliente.entity";
-import { ContaEntity } from "../../../contas/infra/adapters/entities/conta.entity";
+import { UUID } from "crypto";
 
 
 export interface IClienteService {
-    cadastrarCliente(funcionario: any, criarClienteDto: any): Promise<ClienteEntity>;
-    listarTodos(): Promise<ClienteEntity[]>;
-    encontrarPorId(id: string): Promise<ClienteEntity>;
-    atualizarCliente(id: string, updateClienteDto: UpdateClienteDto): Promise<ClienteEntity>;
-    removerCliente(id: string): Promise<void>;
-    adicionarConta(clienteId: string, contas: ContaEntity): Promise<ClienteEntity>;
+    encontrarPorId(id: UUID): Promise<ClienteEntity>;
+    atualizarCliente(id: UUID, updateClienteDto: UpdateClienteDto): Promise<ClienteEntity>;
+    removerCliente(id: UUID): Promise<void>;
+    
 }
+
+export const IClienteService = Symbol('IClienteService');
