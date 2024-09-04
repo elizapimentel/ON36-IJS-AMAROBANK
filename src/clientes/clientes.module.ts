6425b2f694549ff2f613912f4f583b38e38cb411
elizapimentel <elizapimentel@hotmail.com>
2024-09-04 16:59:56 -0300

    refactor: add interfaces and import typeorm entities

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ClientesRepository } from './infra/adapters/outbound/repository/clientes.repository';
-import { Contas } from '../contas/domain/models/conta';
-import { Gerente } from '../funcionarios/domain/models/gerente.entity
-import { FuncionariosRepository } from '../funcionarios/infra/adapters/outbound/repository/funcionario.repository';
 import { ClientesService } from './application/input/clientes.service';
 import { ClientesController } from './infra/adapters/inbound/controller/clientes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Cliente } from './domain/models/cliente';
+import { IClienteService } from './application/input/IClienteService.interface';
+import { ClienteEntity } from './infra/adapters/entities/cliente.entity';
+import { ContaEntity } from '../contas/infra/adapters/entities/conta.entity';
+import { GerenteEntity } from '../funcionarios/infra/adapters/entities/gerente.entity';
 
 @Module({
   controllers: [ClientesController],
-  providers: [ClientesService, ClientesRepository, FuncionariosRepository],
-  exports: [ClientesService],
+  providers: [
+    {
+      provide: IClienteService,
+      useClass: ClientesService,
+    },
+    ClientesRepository,
+  ],
+  exports: [IClienteService, ClientesRepository],
   imports: [
-    TypeOrmModule.forFeature([Cliente, Contas, Gerente])
+    TypeOrmModule.forFeature([ClienteEntity, ContaEntity, GerenteEntity])
   ],
 })
 export class ClientesModule { }
