b3188190de1d08868ed34c0463aaddcb53cb6341
elizapimentel <elizapimentel@hotmail.com>
2024-09-04 16:57:08 -0300

    refactor: change synchronize to true and add a convertion to string of the pwd

--- data-source.ts
+++ data-source.ts
@@ -4,16 +4,18 @@ import { DataSource } from "typeorm";
 
 dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_DATABASE } = process.env;
+
+const dbPassword = String(process.env.DB_PASSWORD);
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: DB_HOST,
   port: parseInt(DB_PORT || "5432"),
   username: DB_USERNAME,
-  password: DB_PASSWORD,
+  password: dbPassword,
   database: DB_DATABASE,
   entities: [__dirname + "/entity/*.ts"],
   migrations: [__dirname + "/migration/*.ts"],
-  synchronize: false,
+  synchronize: true,
 });
